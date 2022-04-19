a35d3a1ea1f981dd8b2709ad2645d46a9eb02165
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-04-19 15:32:40 -0400

    check if we actually are getting the distance from vDec

--- MC/pythia/AMSB.cc
+++ MC/pythia/AMSB.cc
@@ -3,62 +3,66 @@
 using namespace Pythia8;
 
 int main() {
-  // Generator. Shorthand for the event.
-  Pythia pythia;
-  Event& event = pythia.event;
-  // Read in commands from external file.
-  pythia.readFile("AMSB.cmnd");
+    // Generator. Shorthand for the event.
+    Pythia pythia;
+    Event& event = pythia.event;
+    // Read in commands from external file.
+    pythia.readFile("AMSB.cmnd");
 
 
- // Initialize.
- pythia.init();
+    // Initialize.
+    pythia.init();
 
 // Histograms
-Hist decVtx("Chargino decay vertex (mm from origin)", 100, 0., 1000.);
+    Hist decVtx("Chargino decay vertex (mm from origin)", 100, 0., 1000.);
 
 
 
- int nEvent   = pythia.mode("Main:numberOfEvents");
- int nAbort   = pythia.mode("Main:timesAllowErrors");
+    int nEvent   = pythia.mode("Main:numberOfEvents");
+    int nAbort   = pythia.mode("Main:timesAllowErrors");
 // Begin event loop.
- int iAbort = 0;
- for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
+    int iAbort = 0;
+    for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
 
-   // Generate events. Quit if failure.
-   if (!pythia.next()) {
-     if (++iAbort < nAbort) continue;
-       cout << " Event generation aborted prematurely, owing to error!\n";
-       break;
+        // Generate events. Quit if failure.
+        if (!pythia.next()) {
+            if (++iAbort < nAbort) continue;
+            cout << " Event generation aborted prematurely, owing to error!\n";
+            break;
+        }
+
+        // loop for chargino in the event
+        for (int i = 0; i < event.size(); ++i) {
+            int idAbs = event[i].idAbs();
+            if (idAbs == 1000024) {
+                // Separation of chargino decay vertex from the origin
+                // double dist = event[i].vDec().pAbs(); // It give the same result
+                double dist = sqrt(event[i].xDec() * event[i].xDec() +
+                              event[i].yDec() * event[i].yDec() +
+                              event[i].zDec() * event[i].zDec());
+                decVtx.fill( dist);
+                cout << "\n Chargino decay vertex (mm from origin)\n"<< dist << endl;
+
+            }
+
+        }
+
     }
+    // Stats
+    pythia.stat();
 
-    // loop for chargino in the event
-    for (int i = 0; i < event.size(); ++i) {
-      int idAbs = event[i].idAbs();
-      if (idAbs == 1000024) {
-        double dist = event[i].vDec().pAbs();
-        decVtx.fill( dist);
-        cout << "\n Chargino decay vertex (mm from origin)\n"<< dist << endl;
-        
-      } 
-      
-    }
-    
- }
- // Stats
- pythia.stat();
+    // normalize histograms by 1/nEvent
+    decVtx.normalizeSpectrum(nEvent);
+    cout << decVtx << endl;
+    //return 0;
 
- // normalize histrograms by 1/nEvent
- decVtx.normalizeSpectrum(nEvent);
- cout << decVtx << endl;
- //return 0;
-
-  HistPlot hpl("DecayPlot");
-  hpl.frame( "DecayVertex", "Chargino Decay Vertex (500 GeV)", "Distance (mm)","Entries");
-  hpl.add( decVtx, "h,red", "DecayVertex");
-  hpl.plot();
+    HistPlot hpl("DecayPlot");
+    hpl.frame( "DecayVertex", "Chargino Decay Vertex (1000 GeV)", "Distance (mm)","Entries");
+    hpl.add( decVtx, "h,red", "DecayVertex");
+    hpl.plot();
 
 
-  // Done
-  return 0;
+    // Done
+    return 0;
 
 }
