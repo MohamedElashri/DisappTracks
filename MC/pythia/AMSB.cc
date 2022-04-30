abbd2be6f8730c2bc5806cd1d314184b5fcd8168
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-04-30 07:26:07 -0400

    Add code to get number of accepted events that pass 'eta' and 'vertex' cut

--- MC/pythia/AMSB.cc
+++ MC/pythia/AMSB.cc
@@ -7,9 +7,34 @@
 #include "TFile.h"
 #include <TCanvas.h>
 #include <TF1Convolution.h>
-
+#include <vector>
+#include <iostream>
 
 using namespace Pythia8;
+
+
+// My UserHooks class
+
+/*
+class MyUserHooks : public UserHooks {
+
+    public:
+
+    bool canVetoETA() override {return true;}
+
+    bool doVetoETA(const Event& e) override {
+      // Only LHCb acceptance
+      for (const Particle& p : e) {
+        if (p.eta() > 2 && p.eta() < 5) return 0.;
+          return false;
+      }
+      infoPtr->errorMsg("Info: Event vetoed by MyUserHooks");
+      return true;
+    }
+    // only our decay window is allowed
+};
+*/
+
 
 int main() {
     // Generator. Shorthand for the event.
@@ -18,20 +43,28 @@ int main() {
     // Read in commands from external file.
     pythia.readFile("AMSB.cmnd");
 
+    // To use with statistics
+    int nEventAccepted[1] = { 0 };
+    int nEventVetoed[2] = { 0, 0};
+    const char *vetoStr[] = { "|eta|", "dist" };
+
+
+
 
     // Initialize.
     pythia.init();
     //pythia.particleData.listChanged();
 
     // Create file on which histogram(s) can be saved.
-    //TFile* outFile = new TFile("hist.root", "RECREATE");
+    TFile* outFile = new TFile("hist.root", "RECREATE");
     // Book histogram
-    //TH1F *decVtx = new TH1F("decVtx","Chargino decay vertex", 100, -0, 1000);
+    //TH1F *decVtx = new TH1F("decVtx","Chargino decay vertex ", 100, -0, 10000);
+    TH1F *decVtx = new TH1F("decVtx","Chargino decay vertex ", 100, 900, 1000); // for the decay vertex in custom decay window
     // make exponential fit function
     int CHI = 1000024;
     cout << "Lifetime [mm] =" << scientific << pythia.particleData.tau0(CHI) << endl;
 // Histograms
-    Hist decVtx("Chargino decay vertex (mm from origin)", 100., 0., 10000.);
+    //Hist decVtx("Chargino decay vertex (mm from origin)", 100., 0., 10000.);
     //Hist life("Decay lifetime (mm)" ,100,0,100.);
 
 
@@ -41,13 +74,8 @@ int main() {
 // Begin event loop.
     int iAbort = 0;
     for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
+        if (!pythia.next()) continue;
 
-        // Generate events. Quit if failure.
-        if (!pythia.next()) {
-            if (++iAbort < nAbort) continue;
-            cout << " Event generation aborted prematurely, owing to error!\n";
-            break;
-        }
         //life.fill(event[5].tau());
     
         // loop for chargino in the event
@@ -55,45 +83,66 @@ int main() {
             int idAbs = event[i].idAbs();
             double eta = event[i].eta();
             if (idAbs == 1000024) {
+                /*
                 if (eta > 2  && eta < 5 ) {
                 // Separation of chargino decay vertex from the origin
                 // double dist = event[i].vDec().pAbs(); // It give the same result
                 double dist = sqrt(event[i].xDec() * event[i].xDec() +
                               event[i].yDec() * event[i].yDec() +
                               event[i].zDec() * event[i].zDec());
-                decVtx.fill(dist);
-                //decVtx->Fill(dist);
+                //decVtx.fill(dist);
+                decVtx->Fill(dist);
                 //cout << "\n Chargino decay vertex (mm from origin)\n"<< dist << endl;
                 }
-           
-            }
+                */
+        if (event[i].eta() > 2 && event[i].eta() < 5) {
+            nEventVetoed[0]++;
+        }
+        
 
+            double dist = event[i].vDec().pAbs();        
+            // ratio of numberOfEvents to total number of events between 900 mm and 1000 mm
+            if (dist > 900 && dist < 1000) {
+                nEventVetoed[1]++;
+                nEventAccepted[i]++;
+                decVtx->Fill(dist);
+               //int numberOfEvents = 0;
+               //numberOfEvents = numberOfEvents + 1;
+               //cout << "total number of events between 900 mm and 1000 mm = " << numberOfEvents << endl;
+               }            
+        }
         }
 
-    }
+        
+
+    }    
     // Stats
     pythia.stat();
     // applying fit to histogram
     //decVtx->Fit("exp");
+
+    cout << endl << nEvent << " events generated. " << nEventAccepted[0]
+    << " events passed cuts." << endl;
+    
     // Show histogram. Possibility to close it.
-    //decVtx->Draw();
-    //std::cout << "\nDouble click on the histogram window to quit.\n";
-    //gPad->WaitPrimitive();
+    decVtx->Draw();
+    std::cout << "\nDouble click on the histogram window to quit.\n";
+    gPad->WaitPrimitive();
     // Save histogram on file and close file.
-    //decVtx->Write();
-    //delete outFile;
+    decVtx->Write();
+    delete outFile;
 
     // normalize histograms by 1/nEvent
     //decVtx.normalizeSpectrum(nEvent);
-    cout << decVtx << endl;
+    //cout << decVtx << endl;
     //cout << life << endl;
     //return 0;
 
-    HistPlot hpl("DecayVertex");
-    hpl.frame( "DecayVertexPlot", "Chargino Decay (1000 GeV)", "Distance/lifetime (mm)","Entries");
-    hpl.add( decVtx, "h,red", "DecayVertex");
-    hpl.plot();
-
+    //HistPlot hpl("DecayVertex");
+    //hpl.frame( "DecayVertexPlot", "Chargino Decay (700 GeV)", "Distance/lifetime (mm)","Entries");
+    //hpl.add( decVtx, "h,red", "DecayVertex");
+    //hpl.plot();
+    
     //HistPlot hpl("Decaylifetime");
     //hpl.frame( "DecaylifetimePlot", "Chargino Decay Vertex (1000 GeV)", "Life time (mm)","Entries");
     //hpl.add( life, "h,blue", "Decaylifetime");
