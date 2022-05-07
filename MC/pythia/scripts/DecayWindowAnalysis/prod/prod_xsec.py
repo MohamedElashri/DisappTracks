2bca8998a97ec56e076c41ef5ba799b160c6b957
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-05-07 07:33:31 -0400

    change the plot scale to log scale

--- MC/pythia/scripts/DecayWindowAnalysis/prod/prod_xsec.py
+++ MC/pythia/scripts/DecayWindowAnalysis/prod/prod_xsec.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-## for 10 cm 
+
 x1 = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 y1 = [6700,5280,4471,3771,3510,2967,2650,2333,2178,1970]
 xsec_prod = [2.428e-08,1.902e-09,3.969e-10,1.229e-10,4.592e-11,1.984e-11 ,9.366e-12,4.775e-12,2.175e-12,1.328e-12]
-#xsec_prod = [element * 1e-7 for element in xsec_prod]
+#xsec_prod = [element * 1e-3 for element in xsec_prod]
 plt.plot(x1, xsec_prod,marker='o', linestyle='dashdot', color='b', label='Cross Section')
 
-
+plt.yscale('log', nonposy='clip')
 plt.title('Production Cross Section vs Mass')
 plt.xlabel('Mass (GeV)')
 plt.ylabel('Cross Section (mb)')
